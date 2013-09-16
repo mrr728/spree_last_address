ab1b66cc7930405e5cc59ce9b71ea81b59d1c8ed
Rohan Mitchell <rohan@rohanmitchell.com>
2013-09-16 13:43:24 +1000

    Associate user with order before looking up address. This is required when the previous page is a login page (this occurs in a common flow in the checkout process)

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -4,6 +4,8 @@ module Spree
   private
 
     def before_address
+      associate_user
+
       last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email)
       preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, spree_current_user.ship_address if spree_current_user.respond_to?(:bill_address) && spree_current_user.respond_to?(:ship_address)
       @order.bill_address ||= preferred_bill_address || last_used_bill_address || Address.default
